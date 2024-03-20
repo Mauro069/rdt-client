6adb8bcbbb042225f5fa0dee9c2cd3a8716b738e
Mauro Vera <Maurovera069@gmail.com>
2024-03-20 11:00:39 -0300

    Agregue Label al componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,22 @@
 import { InputHTMLAttributes } from 'react'
 import styles from './styles.module.scss'
+import { Label } from '../Label'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+}
 
-export const Input = ({ className, ...inputProps }: InputProps) => {
+export const Input = ({ className, label, ...inputProps }: InputProps) => {
   const inputStyles = `${styles.input} ${className ?? ''}`
+
+  if (label) {
+    return (
+      <div>
+        <Label>{label}</Label>
+        <input className={inputStyles} {...inputProps} />
+      </div>
+    )
+  }
 
   return <input className={inputStyles} {...inputProps} />
 }
